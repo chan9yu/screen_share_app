d9a9f29c223ce3e7536cbfec2d5cb3c6b91ffbdf
chan9yu <dev.cgyeo@gmail.com>
2024-12-22 18:37:57 +0900

    feat: app version 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
+import styled from '@emotion/styled';
+
 import { JoinContainer, RoomContainer } from './components';
 import { useMainService } from './contexts';
 
 export default function App() {
 	const { joined } = useMainService();
 
-	return joined ? <RoomContainer /> : <JoinContainer />;
+	return (
+		<>
+			{joined ? <RoomContainer /> : <JoinContainer />}
+			<VersionTab>v{APP_VERSION}</VersionTab>
+		</>
+	);
 }
+
+const VersionTab = styled.p`
+	position: fixed;
+	left: 12px;
+	bottom: 12px;
+	color: ${({ theme }) => theme.colors.gray[500]};
+	font: ${({ theme }) => theme.typography.M200};
+`;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,5 +17,8 @@ export default defineConfig({
 	server: {
 		host: '0.0.0.0',
 		port: 3035
+	},
+	define: {
+		APP_VERSION: JSON.stringify(process.env.npm_package_version)
 	}
 });
